efbd67529d68faf0affced83eb42f52d7399f29a
Makimi <makimix36@gmail.com>
2026-09-12 22:40:04 +0900

    ユーザー新規登録完了

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+#  skip_before_action :require_login, only: %i[new create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, success: t('users.create.success')
+    else
+      flash.now[:danger] = t('users.create.failure')
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+#  def show
+#  end
+
+
+
+def user_params
+  params.require(:user).permit(:email, :nickname, :password, :password_confirmation)
+end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_validation :set_default_nickname
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:password_digest] }
+  validates :nickname, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true
+
+  private
+
+  def set_default_nickname
+    self.nickname = 'no name' if nickname.blank?
+  end  
 end
