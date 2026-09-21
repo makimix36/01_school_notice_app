a1d7a091e24ba290f0a2c1d6a7a1a040721dfcef
Makimi <makimix36@gmail.com>
2026-09-21 22:06:07 +0900

    お知らせ新規作成登録

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'cloudinary'
 
 # 日本語化
 gem "rails-i18n", "~> 7.0.0"
+gem 'enum_help'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-
-
+  has_many :notifications
+  
   private
 
   def set_default_nickname
