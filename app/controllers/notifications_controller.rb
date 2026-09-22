7d151ac9cb217748dc88b602b0bc3cf7e9f2f425
Makimi <makimix36@gmail.com>
2026-09-22 09:08:53 +0900

    リンクの調整

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -10,7 +10,7 @@ class NotificationsController < ApplicationController
   def create
     @notification = current_user.notifications.build(notification_params)
     if @notification.save
-      redirect_to root_path, notice: "お知らせを作成しました"
+      redirect_to notifications_path, notice: "お知らせを作成しました"
     else
       render :new, status: :unprocessable_entity
     end
